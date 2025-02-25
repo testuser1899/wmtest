c39beb3d6f7dee235c93f6ec8e4aaf4c37e3950b
testuser1899 <worldmarkettk@gmail.com>
2025-02-25 13:59:04 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     testDir: './', // Specify the directory containing your test files
     // Other configuration options
+    use: {
+        slowMo: 1000, // Slows down each operation by 1000ms (1 second)
+      },
+    timeout : 3*60*1000,
+
+    reporter: [['html'], ['line'],],
+
   };
